7c19b913c8a16feb06f0d00a76b7cc4e888c1e0d
Hugo Estrada <hugoestr@cal.berkeley.edu>
2019-05-30 08:37:01 -0400

    publishing song structure. Added draft and publish methods to glass script

--- glass.exs
+++ glass.exs
@@ -11,10 +11,29 @@ defmodule Glass do
   def draft_name(title) do
     "drafts/#{file_name(title)}.html"
   end
+
+  def draft(title) do
+    name = draft_name(title)
+    IO.puts "Creating #{name}"
+
+    System.cmd("cp", ["template.html",name]) 
+  end
+
+  def publish(title) do
+    name = title 
+
+    System.cmd("cp", [name, "index.html"]) 
+    System.cmd("mv", [name,"blog"])
+  end
 end
 
-[title | _rest] = System.argv
-name = Glass.draft_name(title)
+# The proper script starts here
+[action, title | _rest] = System.argv
 
-IO.puts "Creating #{name}"
-System.cmd("cp", ["template.html",name]) 
+case action do
+  "draft"   -> Glass.draft(title)
+  "publish" -> Glass.publish(title)
+  _         -> IO.puts("I don't know how to #{action}")
+end
+
+IO.puts("End of Script")
