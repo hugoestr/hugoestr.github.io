483cc550efadeca84eee278cd7af4769b8a24726
Hugo Estrada <hugoestr@gmail.com>
2019-06-01 09:06:49 -0400

    Adding git automation

--- glass.exs
+++ glass.exs
@@ -17,13 +17,34 @@ defmodule Glass do
     IO.puts "Creating #{name}"
 
     System.cmd("cp", ["template.html",name]) 
+
+    git_add(name)
+    git_commit("Creatingdraft for #{name}")
   end
 
   def publish(title) do
     name = title 
+    published_name = String.replace(name, "drafts", "blog")
 
     System.cmd("cp", [name, "index.html"]) 
     System.cmd("mv", [name,"blog"])
+    
+    git_rm(name) 
+    git_add(published_name) 
+    git_add("index.html") 
+    git_commit("Publishing  #{name}")
+  end
+
+  def git_add_commit(path) do 
+    system.cmd("git", ["add", path]) 
+  end
+
+  def git_rm(path) do
+    system.cmd("git", ["rm", path]) 
+  end
+
+  def git_commit(message) do
+    system.cmd("git", ["commit", "-m", message]) 
   end
 end
 
